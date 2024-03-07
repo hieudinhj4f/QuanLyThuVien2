14b0640c9f1fdd01e065df0f9f3c5c1e37356c96
hieudinhj4f <dinhhieunguyen248@gmail.com>
2024-03-07 13:18:16 +0700

    Chechkout

---
+++ QuanLyThuVien/Admin Mangagement .Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace QuanLyThuVien
+{
+    partial class Admin_Mangagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SideBar = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.HomeButton = new System.Windows.Forms.Button();
+            this.SideBarTimer = new System.Windows.Forms.Timer(this.components);
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.SideBar.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SideBar
+            // 
+            this.SideBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.SideBar.Controls.Add(this.panel2);
+            this.SideBar.Controls.Add(this.panel1);
+            this.SideBar.Controls.Add(this.panel4);
+            this.SideBar.Controls.Add(this.panel3);
+            this.SideBar.Location = new System.Drawing.Point(1, 1);
+            this.SideBar.MaximumSize = new System.Drawing.Size(348, 731);
+            this.SideBar.MinimumSize = new System.Drawing.Size(111, 731);
+            this.SideBar.Name = "SideBar";
+            this.SideBar.Size = new System.Drawing.Size(348, 731);
+            this.SideBar.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.HomeButton);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(107, 103);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(3, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(345, 107);
+            this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(8, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(319, 92);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "      SINH VIÊN";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Location = new System.Drawing.Point(3, 225);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(345, 103);
+            this.panel4.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(8, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(319, 92);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "      SÁCH ";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(3, 334);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(345, 103);
+            this.panel3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(8, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(319, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "      TRẢ MƯỢN";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // HomeButton
+            // 
+            this.HomeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.HomeButton.BackgroundImage = global::QuanLyThuVien.Properties.Resources.icons8_menu_bar_80__2_;
+            this.HomeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.HomeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.HomeButton.FlatAppearance.BorderSize = 0;
+            this.HomeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeButton.Location = new System.Drawing.Point(0, 3);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(104, 97);
+            this.HomeButton.TabIndex = 1;
+            this.HomeButton.UseVisualStyleBackColor = false;
+            this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
+            // 
+            // SideBarTimer
+            // 
+            this.SideBarTimer.Tick += new System.EventHandler(this.SideBarTimer_Tick);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 734);
+            this.splitter1.TabIndex = 2;
+            this.splitter1.TabStop = false;
+            // 
+            // Admin_Mangagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1191, 734);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.SideBar);
+            this.Name = "Admin_Mangagement";
+            this.Text = "Admin_Mangagement";
+            this.SideBar.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel SideBar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button HomeButton;
+        private System.Windows.Forms.Timer SideBarTimer;
+        private System.Windows.Forms.Splitter splitter1;
+    }
+}
---
+++ QuanLyThuVien/Admin Mangagement .cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyThuVien
+{
+    public partial class Admin_Mangagement : Form
+    {
+        //Check SideBar
+        bool SideBarExpand;
+        public Admin_Mangagement()
+        {
+            InitializeComponent();
+        }
+
+        private void SideBarTimer_Tick(object sender, EventArgs e)
+        {
+            if( SideBarExpand )
+            {
+                // if sidebar is expand , minimize 
+                SideBar.Width -= 100;
+                if( SideBar.Width == SideBar.MinimumSize.Width)
+                {
+                    SideBarExpand = false;
+                    SideBarTimer.Stop();
+                }
+            }
+            else
+            {
+                SideBar.Width += 100;
+                if ( SideBar.Width == SideBar.MaximumSize.Width )
+                {
+                    SideBarExpand = true;
+                    SideBarTimer.Stop();
+                }
+            }
+        }
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+            SideBarTimer.Start();
+        }
+    }
+}
